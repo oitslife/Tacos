080a08387b7e46edfd79d4074ce559291f728075
oitslife <worlfdl6920@gmail.com>
2021-06-06 20:23:35 +0900

    taco file start

---
+++ taco-cloud/src/main/java/tacos/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args); //jar파일이 실행될 때 호출되어 실행되는 메서드다. 
+	}
+
+}
